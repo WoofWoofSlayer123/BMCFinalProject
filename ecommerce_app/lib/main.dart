3fcdbaf5f43360c9a6e3a84c8b5d935a6d1fe0c0
WoofWoofSlayer123 <168385518+WoofWoofSlayer123@users.noreply.github.com>
2025-11-04 14:32:13 +0800

    Update main.dart

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:ecommerce_app/screens/auth_wrapper.dart'
+import 'package:ecommerce_app/screens/auth_wrapper.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
